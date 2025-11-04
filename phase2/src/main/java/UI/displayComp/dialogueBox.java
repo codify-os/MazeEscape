6274cc2ad3877674c91b2ab3606d98e14c45f70a
Niyousha Aminafshari <naa71@sfu.ca>
2025-11-04 14:59:12 -0800

    UI:Recent topPanel & dialogeBox edits

--- phase2/src/main/java/UI/displayComp/dialogueBox.java
+++ phase2/src/main/java/UI/displayComp/dialogueBox.java
@@ -1,4 +1,33 @@
 // dialogueBox.java
 // Displays what the character is saying or prompts the game is providing to the character
-// will add an addition buttion for skip in case player does not wanna read the prompts 
+// will add an addition button for skip in case player does not wanna read the prompts 
 // The volume option also controls if the dialogue is read out load 
+
+
+package UI.displayComp;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List; 
+
+public class dialogueBox {
+    // some variable declarations 
+    private static final int Marg = 10; 
+    private static final int cornerR = 12; 
+    private static final int height = 56; 
+
+    private boolean view = false; 
+    private final List<String> line = new ArrayList<>(); // to hold a list of all the dialogue lines 
+    private int index = 0; // to keep track of the current line 
+
+    // Loading a dialogue box with strings + clearing when needed 
+    public void loadLine(String... text) { 
+        line.clear();
+        for (String s : text) if (s != null ) line.add(s); // if line is not empty, adds it to the list 
+        index = 0; 
+        view = !line.isEmpty();
+    }
+
+    // some basic visibility function 
+
+}
--- phase2/src/main/java/UI/displayComp/topPanel.java
+++ phase2/src/main/java/UI/displayComp/topPanel.java
@@ -7,9 +7,6 @@ import java.awt.event.MouseEvent;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import javax.swing.Action;
-
-
 /**
  * 
  * Work in progress topPanel for the initial start up of the game 
@@ -25,7 +22,7 @@ import javax.swing.Action;
 
  // Estimate layout 
  public class topPanel{
-    private static final int Bar_Height     = 48;  // height of the top bar
+    private static final int Bar_Height     = 68;  // height of the top bar
     private static final int Bttn_Width     = 70;  // button width
     private static final int Bttn_Height    = 28;  // button height
     private static final int Bar_Gap_Space  = 12;  // left/right padding
@@ -62,44 +59,47 @@ public enum ActionBar {
      */
     public void draw(Graphics2D bar_graphics, int panelWidth){
         // The back ground colour 
-       bar_graphics.setColor(new Color(0x3B2F2F));
-       bar_graphics.fillRect(0, 0, panelWidth, Bar_Height);
+        bar_graphics.setColor(new Color(0x3B2F2F));
+        bar_graphics.fillRect(0, 0, panelWidth, Bar_Height);
 
         // for nicer icons and icon edges 
-       bar_graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        // setting the starting point for the bar 
-        int x = Bar_Gap_Space;
-        int y = (Bar_Height - Bttn_Height ) / 2; // centering the box 
-
-        // when boxes are empty, clear no action is being taken 
-        box.clear();
-        x = dButton(bar_graphics, x, y, ActionBar.Zoom_In,  "+");
-        x = dButton(bar_graphics, x, y, ActionBar.Zoom_Out, "-");
-        x = dButton(bar_graphics, x, y, ActionBar.Button_Sound,    pause? "▶" : "⏸");
-        x = dButton(bar_graphics, x, y, ActionBar.Button_Pause, pause ? "PLAY" : "PAUSE");
-        x = dButton(bar_graphics, x, y, ActionBar.Button_Sound, mute ? "MUTE" : "SOUND");
-        x = dButton(bar_graphics, x, y, ActionBar.Button_Back,  "BACK");
-
+        bar_graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        // Title and buttons seperation -- title first , Buttons second 
         // adjusing the texts within the boxes/ buttons 
         bar_graphics.setColor(new Color(0xFFFDD0));  // creamy white color
         Font oldFont = bar_graphics.getFont();
-        bar_graphics.setFont(oldFont.deriveFont(Font.BOLD, 16f));
+        bar_graphics.setFont(oldFont.deriveFont(Font.BOLD, 18f));
 
-        String gameTitle = "Dungeon Escape";
+        final String gameTitle = "Dungeon Escape";
         int textWidth = bar_graphics.getFontMetrics().stringWidth(gameTitle);
-        bar_graphics.drawString(gameTitle, (panelWidth - textWidth) / 2, 30);
+        int topP_baseLine = 20; 
+        bar_graphics.drawString(gameTitle, (panelWidth - textWidth) / 2, topP_baseLine);
         bar_graphics.setFont(oldFont);
+
+       // Buttons on botton of the top panel 
+       int x = Bar_Gap_Space;
+       int y = Bar_Height - Bttn_Height - 10; // centering the box 
+
+       // when boxes are empty, clear no action is being taken 
+       box.clear();
+       x = drawsButton(bar_graphics, x, y, ActionBar.Zoom_In,  "+");
+       x = drawsButton(bar_graphics, x, y, ActionBar.Zoom_Out, "-");
+       x = drawsButton(bar_graphics, x, y, ActionBar.Button_Sound,    pause? "▶" : "⏸");
+       x = drawsButton(bar_graphics, x, y, ActionBar.Button_Pause, pause ? "PLAY" : "PAUSE");
+       x = drawsButton(bar_graphics, x, y, ActionBar.Button_Sound, mute ? "MUTE" : "SOUND");
+       x = drawsButton(bar_graphics, x, y, ActionBar.Button_Back,  "BACK");
 
         // Bottom border line bounds 
         bar_graphics.setColor(new Color(0xFFFDD0));
         bar_graphics.drawLine(0, Bar_Height - 1, panelWidth, Bar_Height - 1);
 
         // Right aligned exit button
-        int righttX = panelWidth - Bar_Gap_Space - Bttn_Width;
-        draw_ButtonAt(bar_graphics, rightX, y, ActionBar.Button_EXIT, "Exit");
+        int rightX = panelWidth - Bar_Gap_Space - Bttn_Width;
+        draw_ButtonAt(bar_graphics, rightX, y, ActionBar.Button_EXIT, "EXIT");
         
     }
-
+    
     /**
      * Creating button clicking function 
      * determines which button is pressed 
@@ -113,19 +113,55 @@ public enum ActionBar {
         for (Map.Entry<ActionBar, Rectangle> button : box.entrySet()) {
             if (button.getValue().contains(mouse_x, mouse_y)) {
                 ActionBar click = button.getKey();
-                applyAction(click);
+                pressButton(click);
                 return click;
             }
         }
         return ActionBar.none;
     }
 
+    // using apply action function created to have button pressing affect - might not be needed 
+    private void pressButton (ActionBar click){
+        // creating switch cases to create an action for each button when pressed 
+        // ex) sound volume from mute -> unmute, same goes for other buttosn too 
+        switch(click){ 
+            case Zoom_In -> zoom = Math.min(2.0, zoom + 0.1);
+            case Zoom_Out -> zoom = Math.max(0.5, zoom - 0.1); 
+            case Button_Pause -> pause = !pause; 
+            case Button_Sound -> mute = !mute; 
+            default -> { /* cannot think of anything else for now, might be other future cases */}
+        }
+    }
 
 
-    // Helper fucntion to help draw a button and space it correctly 
-    private int dButton(Graphics2D bar_graphics, int x, int y, ActionBar action, String label) {
+    // Helper function to help draw a button and space it correctly 
+    private int drawsButton(Graphics2D bar_graphics, int x, int y, ActionBar action, String label) {
         draw_ButtonAt(bar_graphics, x, y, action, label);
         return x + Bttn_Width + Bttn_Gap;
     }
 
- }
+    /**
+     * section draws the genuine rectangle, creates one rectangle with the desired affects "Hopefully"
+     * 
+     */
+    private void draw_ButtonAt(Graphics2D bar_graphics, int x, int y, ActionBar action, String label){ 
+        // rectangle shape 
+        Rectangle rectBase = new Rectangle(x, y, Bttn_Width, Bttn_Height); 
+        box.put(action,rectBase); 
+
+        // Button background base 
+        bar_graphics.setColor(new Color(0xC65D57));  // muted tomato red
+        bar_graphics.fillRect(rectBase.x, rectBase.y, rectBase.width, rectBase.height);
+
+        // button border 
+        bar_graphics.setColor(new Color(255, 255, 255, 60)); // hoping to get shadowy mist 
+        bar_graphics.drawRect(rectBase.x, rectBase.y, rectBase.width, rectBase.height);
+
+        // location and of titles and texts on the box 
+        bar_graphics.setColor(new Color(0xFFFDD0));
+        FontMetrics fontSize = bar_graphics.getFontMetrics(); 
+        int textX = rectBase.x + (rectBase.width - fontSize.stringWidth(label))/2; 
+        int textY = rectBase.y + (rectBase.height + fontSize.getAscent() - fontSize.getDescent())/2; 
+        bar_graphics.drawString(label, textX, textY);
+  }
+} 
