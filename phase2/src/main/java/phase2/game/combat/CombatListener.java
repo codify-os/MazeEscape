7805107f36bd991e06bcdf74155ea1359a8422f5
Cody Nguyen <128450026+codify-os@users.noreply.github.com>
2025-11-04 14:47:34 -0800

    implemented combatlistener

--- phase2/src/main/java/phase2/game/combat/CombatListener.java
+++ phase2/src/main/java/phase2/game/combat/CombatListener.java
@@ -4,5 +4,33 @@ package phase2.game.combat;
  * Listener interface for combat events (for UI, analytics, etc.)
  */
 public interface CombatListener {
-    // TODO: Define callback methods for damage, death, and other combat events
+    /**
+     * Called when an entity takes damage
+     * @param target The entity that took damage
+     * @param amount The amount of damage taken
+     * @param source The source of the damage
+     */
+    void onDamage(Damageable target, int amount, DamageSource source);
+
+    /**
+     * Called when an entity dies
+     * @param target The entity that died
+     * @param source The source that killed them
+     */
+    void onDeath(Damageable target, DamageSource source);
+
+    /**
+     * Called when an attack is performed
+     * @param attacker The entity performing the attack
+     * @param target The target of the attack
+     * @param result The result of the attack
+     */
+    void onAttack(Attacker attacker, Damageable target, AttackResult result);
+
+    /**
+     * Called when an entity is healed
+     * @param target The entity that was healed
+     * @param amount The amount healed
+     */
+    void onHeal(Damageable target, int amount);
 }
