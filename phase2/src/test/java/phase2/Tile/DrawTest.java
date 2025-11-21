6eff8a1d2282cb81f8ce19f7a9ea764b9a8104f3
Joseph Trieu <joseph_trieu@sfu.ca>
2025-11-20 21:44:26 -0800

    Update and rename DrawTests.java to DrawTest.java

--- phase2/src/test/java/phase2/Tile/DrawTest.java
+++ phase2/src/test/java/phase2/Tile/DrawTest.java
@@ -11,7 +11,7 @@ import java.awt.image.BufferedImage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class DrawTests {
+public class DrawTest {
 
     GamePanel gp;
     TileManager tm;
